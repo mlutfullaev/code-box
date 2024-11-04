5f56b6c415ba0739d3fd70d8b44088c407dcd36e
magalutfullaev <mhoja9494@gmail.com>
2024-11-04 15:46:43 +0500

    basic setup

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,19 @@
 <script lang="ts" setup="">
+definePageMeta({
+  title: 'CodeBOX'
+})
 </script>
 
 <template>
-  <div>
-    Hellon't World
-  </div>
+  <main class="h-screen bg-primary flex items-center justify-center gap-10 flex-col">
+    <h1 class="text-white text-center text-4xl">CodeBOX</h1>
+    <router-link
+      to="/components"
+      class="bg-white py-2 px-10 rounded text-primary"
+    >
+      Go to components
+    </router-link>
+  </main>
 </template>
 
 <style lang="scss" scoped>
